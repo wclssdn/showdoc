50c0c5a2ebce55b82fd8590c204b49dd60ca045a
wclssdn <ssdn@vip.qq.com>
2016-03-25 16:17:00 +0800

    Update config.php
    add socket config item

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -7,6 +7,7 @@ return array(
 	'DB_USER'   => 'showdoc', 
 	'DB_PWD'    => 'showdoc123456',
 	'DB_PORT'   => 3306, // 端口
+	'DB_SOCKET' => '', // unix socket
 	'DB_PREFIX' => '', // 数据库表前缀
 	'DB_CHARSET'=> 'utf8', // 字符集
 	'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
